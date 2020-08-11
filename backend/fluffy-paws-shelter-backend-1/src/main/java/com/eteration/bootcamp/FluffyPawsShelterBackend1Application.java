cec94d53b81b08268c65f8a51bd84ab9f6eb1c44
Özge Kocaoğlu <38084944+OzgeKocaoglu@users.noreply.github.com>
2020-08-11 20:08:08 +0300

    User Backend Init
    JPA Entity + Repository

---
+++ backend/fluffy-paws-shelter-backend-1/src/main/java/com/eteration/bootcamp/FluffyPawsShelterBackend1Application.java
@@ -0,0 +1,13 @@
+package com.eteration.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluffyPawsShelterBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluffyPawsShelterBackend1Application.class, args);
+	}
+
+}
